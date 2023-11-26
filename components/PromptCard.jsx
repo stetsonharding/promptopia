f31d65186e829f9848dcd64682a9842b17c7afa8
stetson <stetson2001@msn.com>
2023-11-26 09:28:23 -0800

    Prompt Cards finished with copy functionality.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,7 +9,17 @@ import { usePathname, useRouter } from 'next/navigation'
 const PromptCard = ({key, post, handleClickTag, handleEdit, handleDelete}) => {
 
 
-  console.log(post)
+const [isPostCopied, setIsPostCopied] = useState("false")
+
+
+const handleCopy = () => {
+  setIsPostCopied(post.prompt)
+  navigator.clipboard.writeText(post.prompt)
+  setTimeout(() => {
+    isPostCopied("")
+  }, 3000);
+}
+
 
   return (
   <div className="prompt_card">
@@ -27,10 +37,17 @@ const PromptCard = ({key, post, handleClickTag, handleEdit, handleDelete}) => {
     <h3 className="text-gray-900 font-satoshi font-bold">{post.creator.username}</h3>
     <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
     </div>
-    <div className="copy_btn">
-      {/* //Copy image here */}
+    <div className="copy_btn" onClick={handleCopy}>
+      <Image 
+      src={isPostCopied === post.prompt ? `/assets/icons/tick.svg` : "/assets/icons/copy.svg"}
+      width={12}
+      height={12}
+      />
     </div>
   </div>
+
+  <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+  <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleClickTag && handleClickTag(post.tag)} > {post.tag}</p>
   </div>
   )
 }
