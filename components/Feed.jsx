893a0bc9e2b7fabd62d59c416dd657ea52c777cd
stetson <stetson2001@msn.com>
2023-12-11 07:56:41 -0800

    filtering searched prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,16 +4,18 @@ import React, { useState, useEffect } from "react";
 
 import PromptCard from "./PromptCard";
 
-const PromptCardList = ({ data, handleClickTag }) => {
+const PromptCardList = ({ data, handleClickTag, searchedResults }) => {
+  const promptsToRender = searchedResults.length > 0 ? searchedResults : data;
+
   return (
     <div className="mt-16 prompt-layout">
-      {data.map((post, index) => (
-        <PromptCard  
-        key={post._id}
-        post={post}
-        handleClickTag={handleClickTag}
+      {promptsToRender.map((post, index) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleClickTag={handleClickTag}
         />
-    ))}
+      ))}
     </div>
   );
 };
@@ -21,9 +23,26 @@ const PromptCardList = ({ data, handleClickTag }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-  
-  const handleSearchChange = async (e) => {};
-  
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const handleSearchChange = async (e) => {
+    setSearchText(e.target.value);
+    let searchedPrompts = filteredPrompts(searchText);
+
+    setSearchedResults(searchedPrompts);
+  };
+
+  const filteredPrompts = (searchText) => {
+    let regex = new RegExp(searchText, "i");
+
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
   useEffect(() => {
     //Make get request to api to fetch posts
     const fetchPosts = async () => {
@@ -33,15 +52,10 @@ const Feed = () => {
         setPosts(data);
       } catch (error) {
         console.log(error);
-        
       }
     };
-    
-    
     fetchPosts();
   }, []);
- 
-
 
   return (
     <section className="feed">
@@ -52,11 +66,16 @@ const Feed = () => {
           className="search_input"
           placeholder="Search for a tag or a username"
           value={searchText}
-          onChange={(e) => handleSearchChange(e)}
+          onChange={handleSearchChange}
         />
+     
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList
+        searchedResults={searchedResults}
+        data={posts}
+        handleTagClick={() => {}}
+      />
     </section>
   );
 };
