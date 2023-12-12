e45201d8c25b1935c5303af5450eaae74f1b1e9c
stetson <stetson2001@msn.com>
2023-12-12 07:42:05 -0800

    Comments.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,36 +24,43 @@ const Feed = () => {
   const [posts, setPosts] = useState([]);
 
   //Search states
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [searchedResults, setSearchedResults] = useState([]);
-  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
-
-
+  //Function to handle search changes
   const handleSearchChange = (e) => {
+    //Clear timer
     clearTimeout(searchTimeout);
+    //Update users search value
     setSearchText(e.target.value);
 
-     // debounce method
-  setSearchTimeout(
+    // debounce method - used for not searching on every char typed.
+    //After user has stopped typing for .5 seconds the filtering will begin
+    setSearchTimeout(
       setTimeout(() => {
         const searchResult = filteredPrompts(e.target.value);
         setSearchedResults(searchResult);
       }, 500)
     );
   };
-  
 
- 
+  //Function to filter prompts based on username, tag, or prompt
   const filteredPrompts = (searchText) => {
     let regex = new RegExp(searchText, "i");
-
     return posts.filter(
       (item) =>
         regex.test(item.creator.username) ||
         regex.test(item.tag) ||
         regex.test(item.prompt)
     );
+  };
+
+  //Function to search prompts based on tag user clicked on
+  const handleClickTag = (tag) => {
+    setSearchText(tag);
+    let searchedTags = filteredPrompts(tag);
+    setSearchedResults(searchedTags);
   };
 
   useEffect(() => {
@@ -81,13 +88,12 @@ const Feed = () => {
           value={searchText}
           onChange={handleSearchChange}
         />
-     
       </form>
 
       <PromptCardList
         searchedResults={searchedResults}
         data={posts}
-        handleTagClick={() => {}}
+        handleClickTag={handleClickTag}
       />
     </section>
   );
